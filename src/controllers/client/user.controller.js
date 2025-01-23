ff5badd4b629093283a1b02d8821bad257c53d92
BachLaToi <Bachdeptrai04@gmail.com>
2025-01-23 15:56:53 +0700

    profile

--- src/controllers/client/user.controller.js
+++ src/controllers/client/user.controller.js
@@ -228,3 +228,29 @@ module.exports.resetPasswordPatch = async (req, res) => {
     });
   }
 };
+
+// [GET] /v1/users/profile
+module.exports.profile = async (req, res) => {
+  try {
+    const userId = req.user.userId; 
+
+    
+    const user = await User.findById(userId).select("-password"); 
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found!",
+      });
+    }
+
+    res.status(200).json({
+      message: "User profile retrieved successfully!",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};

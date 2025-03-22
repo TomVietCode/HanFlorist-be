ce1205c349af1525bf1b3095cdd26bef9cf3c964
Cuong Nguyen <mcuong04.work@gmail.com>
2025-03-23 02:06:13 +0700

    Feat: Cart service

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -2,18 +2,30 @@ const mongoose = require("mongoose")
 
 const cartSchema = mongoose.Schema(
   {
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true },
     products: [
       {
-        product_id: String,
+        _id: false,
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product'
+        },
         quantity: Number,
+        subTotal: {
+          type: Number,
+          min: [0, "Total amount must be greater than or equal to 0"]
+        } //price * quantity * (1 - discountPercentage/100)
       },
     ],
+    totalAmount: {
+      type: Number,
+      min: [0, "Total amount must be greater than or equal to 0"]
+    },
     expireAt: {
       type: Date,
       expires: 0
     },
-  },
+  }, { timestamps: true }
 )
 
 const Cart = mongoose.model("Cart", cartSchema, "carts")
